4ce19edee9a3140812013960b2f739a0ada4dae4
seohyun9672 <ajeong4@my.bcit.ca>
2022-11-07 09:46:51 -0800

    initial commit

--- public/js/index.js
+++ public/js/index.js
@@ -1,16 +1,18 @@
 var h1 = document.querySelector("h1");
 var h2 = document.createElement("h2");
 var img = document.createElement("img");
-var button = document.querySelector(".btn");
-var input = document.querySelectorAll("input");
+var button = document.querySelector("button");
+var input = document.querySelector("input");
 var container = document.querySelector("div");
 var icon = document.createElement("i");
-var backButton = document.querySelector(".btnhidden");
+var backButton = document.createElement("button");
 const body = document.querySelector("body");
 var p = document.createElement("p");
-var pageNum = 1;
 
-document.addEventListener("DOMContentLoaded", loadPage("index"));
+var page = 1;
+var pageCount = 3;
+
+document.addEventListener("DOMContentLoaded", loadFirstPage("index"));
 
 function changeText(h1Text, h2Text) {
   h1.innerText = h1Text;
@@ -22,39 +24,9 @@ function addImg(src) {
   img.src = src;
 };
 
-function loadPage() {
-  document.querySelector(".textcont").appendChild(h2);
-
-  changeText("Star Wheel", "Horoscope Calculator");
-  addImg("/images/WindRose.svg");
-  button.innerText = "Begin";
-  button.classList.add("begin");
-  document.querySelector(".btncont").classList.add("center");
-  button.addEventListener("click", function () {
-    changePage(1);
-  });
-  // newButton.addEventListener("click", function () {
-  //   changePage(pageNum);
-  // });
-};
-
-function previousPage() {
-  backButton.innerHTML = '<i class="fa fa-chevron-left" aria-hidden="true"></i> Back'
-  backButton.addEventListener("click", function () {
-    changePage(1)
-  });
-};
-
-function nextPage() {
-  button.innerHTML = 'Continue <i class="fa fa-chevron-right" aria-hidden="true" />'
-  button.addEventListener("click", function () {
-    changePage(pageNum + 1)
-  });
-};
-
 function backToMain() {
   button.classList.add("home");
-  button.innerHTML = "Back to Main";
+  button.innerText = "Back to Main";
   button.classList.remove("continue");
   backButton.remove();
   button.addEventListener("click", function () {
@@ -62,85 +34,165 @@ function backToMain() {
   });
 };
 
-function changePage(pageNum) {
-  img.remove();
+function previousPage() {
+  backButton.addEventListener("click", function () {
+    page = (page + 1) - (page - 1)
+    display(page);
+  });
+};
+
+function nextPage() {
+  button.addEventListener("click", function () {
+    page = page + 1
+    display(page);
+  });
+};
+
+function loadFirstPage() {
+  document.querySelector(".textcont").appendChild(h2);
+
+  changeText("Star Wheel", "Horoscope Calculator");
+  addImg("/images/WindRose.svg");
+  button.innerText = "Begin";
+  button.classList.add("begin");
+  document.querySelector(".btncont").classList.add("center");
+
+  button.addEventListener("click", function () {
+    display(1);
+  });
+};
+
+function display(page) {
   h2.remove();
-  button.classList.remove("begin");
-  button.classList.add("continue");
-  
-  /* INPUT VALUE*/
-  var nameInput = document.querySelector(".inputcont").firstElementChild;
-  var signInput = document.querySelector(".inputcont").lastElementChild;
-  var userName = nameInput.value;
-  var signName = signInput.value;
 
-  /* BACKBUTTON */
-  if(backButton.classList.contains("btnhidden")){
-    backButton.classList.replace("btnhidden","back");
-  } else {
-    backButton.classList.replace("back","btnhidden");
-  }
+  button.classList.replace("begin", "continue");
+  button.innerHTML = 'Continue <i class="fa fa-chevron-right" aria-hidden="true" />'
+  document.querySelector(".btncont").appendChild(backButton);
+  document.querySelector(".btncont").insertBefore(backButton, button)
 
-  if (pageNum === 1) {
-    
-    /* Hide backButton */
-    backButton.classList.replace("back","btnhidden");
-    
+  backButton.classList.add("back");
+  backButton.innerHTML = '<i class="fa fa-chevron-left" aria-hidden="true"></i> Back'
+  document.querySelector(".btncont").style.justifyContent = "space-between";
+
+  document.querySelector(".container").classList.replace('center', 'start');
+
+  nextPage();
+  previousPage();
+
+  if (page === 1) {
     changeText("Enter Your Name");
-
-    /* NAME INPUT */
-    nameInput.setAttribute("type", "text");
-    nameInput.classList.remove("hidden");
-    // input.setAttribute("class", "username");
-    // document.querySelector("form").appendChild(input);
-    document.querySelector(".inputcont").style.display = "flex";
-    nextPage();
-
+    input.setAttribute("type", "text");
+    document.querySelector(".inputcont").classList.replace("hidden", "center");
+    img.classList.add("hidden");
   };
 
-  if (pageNum === 2) {
-    /* NAME INPUT */
-    nameInput.classList.add("hidden");
-
-    /* SIGN INPUT */
-    signInput.setAttribute("type", "date");
-    signInput.classList.remove("hidden");
-
+  if (page === 2) {
     changeText("Select your Date of Birth");
-    // input.setAttribute("type", "date");
-    // input.classList.replace("username","date");
-    previousPage();
-    button.addEventListener("click", function () {
-      showResults(userName,signName);
-      backToMain();
-    });
+    input.setAttribute("type", "date");
+    document.querySelector(".inputcont").classList.replace("hidden", "center");
+    img.classList.add("hidden");
+  };
 
-    console.log(userName,signName);
+  if (page === 3) {
+    img.classList.remove("hidden");
+    addImg("/images/signs/leo.svg");
+    // document.querySelector(".imgcont").after("textcont");
+    document.querySelector(".container").style.gap = "40px"
+
+    var div = document.createElement("div")
+    document.querySelector(".container").appendChild(div);
+
+    div.classList.add("zodiac-date");
+    div.innerText = "Jan 20 - Feb 18";
+
+    document.querySelector(".btncont").classList.add("hidden");
+
+    document.querySelector(".container").classList.replace("center", "start");
+    document.querySelector(".inputcont").classList.replace("center", "hidden");
+
+    document.querySelector(".container").appendChild(p);
+    p.innerText = 'Despite the "aqua" in its name, Aquarius is actually the last air sign of the zodiac. Innovative, progressive, and shamelessly revolutionary. Accordingly, Aquarius is the most humanitarian astrological sign.'
+    // showResults();
+
   };
 };
 
-function showResults(userName,signName) {
+input.addEventListener("input", (event) => {
+  var userName = event.target.value
+  changeText("Hi " + userName + ", " + "You are a " + "signName");
 
-  changeText("Hi " + userName + ", " + "You are a " + signName);
+  console.log(userName)//Gets a property of the element
+})
 
-  document.querySelector(".imgcont").after(".textcont");
+function storeInput() {
 
-  addImg("/images/signs/leo.svg");
+}
 
-  var div = document.createElement("div")
-  document.querySelector(".container").appendChild(div);
 
-  div.classList.add("zodiac-date");
-  div.innerText = "Jan 20 - Feb 18";
 
-  document.querySelector(".inputcont").style.display = "none";
 
-  document.querySelector(".container").classList.add("start");
-  document.querySelector(".container").classList.remove("center");
 
-  document.querySelector(".container").appendChild(p);
-  p.innerText = 'Despite the "aqua" in its name, Aquarius is actually the last air sign of the zodiac. Innovative, progressive, and shamelessly revolutionary. Accordingly, Aquarius is the most humanitarian astrological sign.'
-};
+
+// function changePage() {
+
+
+
+//   /* INPUT VALUE*/
+
+//   // /* BACKBUTTON */
+//   // if (backButton.classList.contains("back")) {
+//   //   backButton.classList.replace("back", "back");
+//   // } else {
+//   //   backButton.classList.replace("back", "back");
+//   // }
+
+//   if (page === 1) {
+//     changeText("Enter Your Name");
+//     img.remove();
+
+
+//     /* Hide backButton */
+//     // backButton.classList.replace("back", "back");
+
+//     /* NAME INPUT */
+//     // nameInput.setAttribute("type", "text");
+//     // signInput.classList.add("hidden");
+//     // storeInput()
+//   };
+
+//   if (page === 2) {
+//     img.remove();
+//     changeText("Select your Date of Birth");
+//     input.setAttribute("type", "date");
+
+
+//     // nameInput.classList.add("hidden");
+//     // signInput.setAttribute("type", "date");
+//     // signInput.classList.remove("hidden");
+//   };
+
+//   if (page === 3) {
+//     addImg("/images/signs/leo.svg");
+//     // document.querySelector(".imgcont").after("textcont");
+//     document.querySelector(".inputcont").remove();
+//     document.querySelector(".container").style.gap ="40px"
+
+//     var div = document.createElement("div")
+//     document.querySelector(".container").appendChild(div);
+
+//     div.classList.add("zodiac-date");
+//     div.innerText = "Jan 20 - Feb 18";
+
+
+//     document.querySelector(".container").classList.replace("center", "start");
+
+//     document.querySelector(".container").appendChild(p);
+//     p.innerText = 'Despite the "aqua" in its name, Aquarius is actually the last air sign of the zodiac. Innovative, progressive, and shamelessly revolutionary. Accordingly, Aquarius is the most humanitarian astrological sign.'
+//     showResults()
+//   }
+// };
+
+
 
 //         // <i class="fa fa-chevron-left" aria-hidden="true"></i>
 //         // <i class="fa fa-chevron-right" aria-hidden="true"></i>
